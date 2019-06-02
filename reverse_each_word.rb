2bd4148da11d582f9de1343e3ea5f19e77347e95
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 06:17:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   array = string.split(' ')
-  newarray = array.map {|x| x.reverse!}
+  newarray = array.collect {|x| x.reverse!}
   returnstring = newarray.join(' ')
   return returnstring
 end
